248b14a28ffb13231bf8a531c4fe8d6674edf338
Abhishek Swamy <swamy.aman@outlook.com>
2023-04-24 13:38:44 +1200

    Updated

--- tests/test1.js
+++
@@ -1,8 +0,0 @@
-fixture('Getting Started')
-    .page('https://devexpress.github.io/testcafe/example');
-
-test('My first test', async t => {
-    await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button');
-});
