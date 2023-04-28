137c2a06a3a7a9c7cc8dca86b094d5e6fc27c619
Abhishek Swamy <swamy.aman@outlook.com>
2023-04-29 01:09:36 +1200

    Updated testcafe.config.js

--- testcafe.config.js
+++ testcafe.config.js
@@ -3,4 +3,9 @@ module.exports = {
     username: 'Admin',
     password: 'admin123',
   },
+  // browsers: ['chrome', 'firefox'], // Add the browsers here
+  // reporter: [
+  //   { name: 'spec' },
+  //   { name: 'html', output: 'reports/report.html' }, // Add the report format and output directory here
+  // ],
 };
